c16165055953789d884a60e0baad2522f31990e6
Lin <893897295@qq.com>
2021-12-27 14:50:55 +0800

    add highlight status

--- device.go
+++ device.go
@@ -1,5 +1,6 @@
 /*
 设备信息
+ver 1.2 拆分Color为高亮+颜色代码
 ver 1.1 增加额外时间戳、原始坐标
 ver 1.0
 */
@@ -9,6 +10,7 @@ import (
 	"encoding/json"
 	"errors"
 	"math"
+	"strconv"
 )
 
 /*
@@ -51,6 +53,83 @@ type Device struct {
 	T3     int64   `json:"t3"` //设备消息接收时间2
 	T4     int64   `json:"t4"` //设备消息转发时间2
 	T5     int64   `json:"t5"` //设备消息接收时间5
+}
+
+/**
+计算状态值（替换s的指定位）
+digit: 位数
+value: 值
+*/
+func SetStatus(s int, digit int, value int) (int, error) {
+	if s < 0 {
+		return 0, errors.New("invalid status code: " + strconv.Itoa(s))
+	}
+	if value > 10 || value < 0 {
+		return 0, errors.New("invalid status value: " + strconv.Itoa(value))
+	}
+	if digit < 0 || digit > 9 { //预留10个状态位
+		return 0, errors.New("invalid status digit: " + strconv.Itoa(digit))
+	}
+	var exp = int(math.Pow10(digit))
+
+	s_rem := s % exp         //余数
+	s_main := s / (exp * 10) //前边几位
+	//s_digit := s/exp - s_main*10 //指定位
+	newS := (s_main*10+value)*exp + s_rem
+	return newS, nil
+}
+
+/**
+计算状态值（替换s的指定位）
+digit: 位数
+value: 值
+*/
+func GetStatus(s int, digit int) (int, error) {
+	if s < 0 {
+		return 0, errors.New("invalid status code: " + strconv.Itoa(s))
+	}
+	if digit < 0 || digit > 9 { //预留10个状态位
+		return 0, errors.New("invalid status digit: " + strconv.Itoa(digit))
+	}
+	var exp = int(math.Pow10(digit))
+	s_main := s / (exp * 10)     //前边几位
+	s_digit := s/exp - s_main*10 //指定位
+	return s_digit, nil
+}
+
+/**
+设置是否高亮（Color的倒数第二位）
+highlight: 是否高亮
+返回：错误
+*/
+func (device *Device) SetHighlight(highlight bool) error {
+	var value int = 0
+	if highlight {
+		value = 1
+	}
+	newS, err := SetStatus(device.Color, 1, value)
+	if err != nil {
+		return err
+	}
+	device.Color = newS
+	return nil
+}
+
+/**
+设置颜色代码（Color的最低位）
+color: 颜色代码，0-9，0一般为默认色
+返回：错误
+*/
+func (device *Device) SetColor(color int) error {
+	if color < 0 || color > 9 {
+		return errors.New("invalid color code: " + strconv.Itoa(color))
+	}
+	newS, err := SetStatus(device.Color, 0, color)
+	if err != nil {
+		return err
+	}
+	device.Color = newS
+	return nil
 }
 
 /**
